f14b9713bb4964d448d0d7a38d7cc935584ad2d7
baonguyexn <baonguyen130402@gmail.com>
2024-12-04 15:00:06 +0700

    doc: deploy by vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/card-fetching_app/",
   server: {
     watch: {
       usePolling: true,
